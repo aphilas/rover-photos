1aa469b6901cc82c2fe29b9c2c02e0e1a3ffe43d
Neville Omangi <nevomangi@gmail.com>
2019-06-24 14:03:28 +0300

    fix port issues

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 import express from 'express'
 
+const PORT = process.env.PORT || 5000
 let app = express()
 
 app.get('/', (req, res) => {
   res.send('Hello from Rover Photos!')
 })
 
-const port = 3000
-
-app.listen(port)
-
-
-
+app.listen(PORT, () => console.log(`Listening on port ${ PORT }`))
