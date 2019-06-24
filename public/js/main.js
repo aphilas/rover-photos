75e75659771b1a8d5ec3e9cf82be2c709d973202
Neville Omangi <nevomangi@gmail.com>
2019-06-24 18:17:32 +0300

    render images from links

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,35 @@
 const formEl = document.getElementById('photos-form')
+const imagesEl = document.getElementById('images')
+
 const endpoint = 'http://localhost:5000/api'
+
+const renderImages = (links) => {
+  console.log(`Your search returned ${ links.length } photos.`)
+
+  imagesEl.innerHTML = ''
+
+  const fragment = document.createDocumentFragment()
+  links.forEach(link => {
+    let image = document.createElement('img')
+    image.src = link
+    image.style.maxWidth = '200px'    
+    fragment.appendChild(image)
+  })
+
+  imagesEl.appendChild(fragment)
+}
 
 const fetchLinks = async (params) => {
   const url = new URL(endpoint)
 
   url.search = params
-  console.log(url.toString())
 
   let response = await fetch(url, 
     {headers: { 'Content-Type': 'application/x-www-form-urlencoded'}})
     .catch(err => console.log(err))
-    
-  let data = await response.json()
 
-  console.log(data)
+  let links = await response.json()
+  renderImages(links)
 }
 
 const handleSubmit = async (event) => {
