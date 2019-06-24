546c2d9910e417aef6e8ed0f23197d17b60bbabb
Neville Omangi <nevomangi@gmail.com>
2019-06-24 14:58:57 +0300

    add api params

--- controllers/api.controller.js
+++ controllers/api.controller.js
@@ -1,5 +1,16 @@
 const apiController = (req, res) => {
-  res.send('Hi from the api!')
+  /* 
+    request params:
+
+    sol (rotation or day from landing) - 0 ...
+    camera - FHAZ, RHAZ, MAST, CHEMCAM, MAHLI, MARDI, NAVCAM, [PANCAM, MINITES]
+
+  */
+
+  const sol = req.params.sol || 0
+  const camera = req.params.camera || 'navcam'
+
+  res.send(`You have requested for photos from camera ${ camera } and sol ${ sol }`)
 }
 
 export { apiController }
