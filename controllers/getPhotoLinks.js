40f62d58be39ef47190b27a85e8665841b82c5d2
Neville Omangi <nevomangi@gmail.com>
2019-06-25 19:30:46 +0300

    fix blank response error

--- controllers/getPhotoLinks.js
+++ controllers/getPhotoLinks.js
@@ -5,9 +5,9 @@ const getPhotoLinks = async (sol, camera) => {
   let linksArr = []
 
   const response = await fetch(endpoint).catch(err => console.log(err))
-  const data = await response.json()
+  const data = await response.json().catch(err => console.log(err))
 
-  data.photos.forEach(photo => linksArr.push(photo.img_src))
+  if (data.photos) data.photos.forEach(photo => linksArr.push(photo.img_src))
 
   return linksArr
 }
