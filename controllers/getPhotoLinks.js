7d2a7c2b75a4f7a077cc8447c0c519ebcb178232
Neville Omangi <nevomangi@gmail.com>
2019-06-25 19:51:34 +0300

    convert http links to https

--- controllers/getPhotoLinks.js
+++ controllers/getPhotoLinks.js
@@ -7,7 +7,13 @@ const getPhotoLinks = async (sol, camera) => {
   const response = await fetch(endpoint).catch(err => console.log(err))
   const data = await response.json().catch(err => console.log(err))
 
-  if (data.photos) data.photos.forEach(photo => linksArr.push(photo.img_src))
+  if (data.photos) data.photos.forEach(photo => {
+    let link = photo.img_src
+    // convert http links to https
+    if (link.match(/^http:/)) link = "https" + link.substring(4)
+
+    linksArr.push(link)
+  })
 
   return linksArr
 }
